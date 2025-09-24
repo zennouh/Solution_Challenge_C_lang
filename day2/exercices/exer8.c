f51cd27b58528a5f523eff39f9e709c0ffd4800a
nouh <nouh@gmail.com>
2025-09-24 19:26:53 +0100

    finish exercice 8

--- day2/exercices/exer8.c
+++ day2/exercices/exer8.c
@@ -4,6 +4,27 @@
 
 void main()
 {
-    int numberone, numbertwo;
+    int number;
     char input[4];
+    char extras[4];
+    printf("Enter a Number: ");
+    fgets(input, 4, stdin);
+    int v = sscanf(input, "%d %c", &number, &extras);
+    if (v != 1)
+    {
+        printf("invalid input");
+        return;
+    }
+    for (int i = 1; i < number; i++)
+    {
+        int carre = i * i;
+        if (carre <= number)
+        {
+            printf("%d\n", carre);
+        }
+        else
+        {
+            return;
+        }
+    }
 }
