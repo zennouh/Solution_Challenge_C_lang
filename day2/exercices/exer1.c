417248f34217ea999b6d0c192a06250e22587153
nouh <nouh@gmail.com>
2025-09-24 11:08:56 +0100

    add some feature to exercice 1

--- day2/exercices/exer1.c
+++ day2/exercices/exer1.c
@@ -13,14 +13,25 @@ void main()
     char numArticle[6];
     char chifferDeControle[2];
     printf("enter le l'isbn:");
-    fgets(input, sizeof(input), stdin);
-    if (strlen(input) != 17)
+    do
     {
-        printf("isbn non valide, try aggain");
-    }
-    else
-    {
-        int v = sscanf(input, "%3s-%1s-%3s-%5s-%1s", &prefix, &groupe, &codeEditeur, &numArticle, &chifferDeControle);
-        printf("Préfixe GS1: %s\nIdentifiant de groupe: %s\nCode de l'éditeur:%s\nNuméro d'article: %s\nChiffre de contrôle: %s", prefix, groupe, codeEditeur, numArticle, chifferDeControle);
-    }
+
+        fgets(input, sizeof(input), stdin);
+        if (strlen(input) != 17)
+        {
+            printf("isbn non valide, try aggain: \n");
+            continue;
+        }
+        else
+        {
+            int v = sscanf(input, "%3s-%1s-%3s-%5s-%1s", &prefix, &groupe, &codeEditeur, &numArticle, &chifferDeControle);
+            if (v != 5)
+            {
+                printf("l' isbn n'est pas valid\n");
+                continue;
+            }
+            printf("Préfixe GS1: %s\nIdentifiant de groupe: %s\nCode de l'éditeur:%s\nNuméro d'article: %s\nChiffre de contrôle: %s", prefix, groupe, codeEditeur, numArticle, chifferDeControle);
+            break;
+        }
+    } while (1);
 }
